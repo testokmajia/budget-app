d9e8ca9cea155cbb3a294583884d51bb580e2377
yangxy <176235759@163.com>
2026-05-31 02:18:16 +0800

    修改前端代码编译路径：由dist改为frontend

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: 'frontend',
+  },
   server: {
     port: 5173,
     proxy: {
